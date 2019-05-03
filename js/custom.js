a2654694f9af278e6fd394f62cd543fdb29a3c94
Ptashkina Liliya <efekta9@gmail.com>
2019-05-03 16:51:23 +0300

    no message

--- js/custom.js
+++ js/custom.js
@@ -19,5 +19,40 @@ $(document).ready(function(){
 	    }, 800);
 	    return false;
 	  	});
+
+
+	$('.slider').slick({
+	  dots: false,
+	  arrows: true,
+	  nextArrow: '<img class="arrow arrow__next" src="img/right.png">',
+	  prevArrow: '<img class="arrow arrow__prev" src="img/left.png">',
+	  adaptiveHeight: true,
+	  slidesToShow: 4,
+	  slidesToScroll: 1,
+	  responsive: [
+	    {
+	      breakpoint: 1024,
+	      settings: {
+	        slidesToShow: 2,
+	        infinite: true,
+	        arrows: true
+	      }
+	    },
+	    {
+	      breakpoint: 600,
+	      settings: {
+	        slidesToShow: 1,
+	        arrows: true
+	      }
+	    },
+	    {
+	      breakpoint: 480,
+	      settings: {
+	        slidesToShow: 1,
+	        arrows: true
+	      }
+	    }
+	  ]
+	});
 }); //end document ready function
 
