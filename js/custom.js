f8943b53ff12297730771f88b8880c07d929bec1
Ptashkina Liliya <efekta9@gmail.com>
2019-05-20 16:22:26 +0300

    air datepicker

--- js/custom.js
+++ js/custom.js
@@ -280,6 +280,25 @@ $(document).ready(function(){
 	  }
 	});
 
+	//DatePicker
+
+$('#example-show-hide-callbacks').data('datepicker')
+$('#example-show-hide-callbacks').datepicker({
+    onShow: function(dp, animationCompleted){
+        if (!animationCompleted) {
+            log('start showing')
+        } else {
+            log('finished showing')
+        }
+    },
+    onHide: function(dp, animationCompleted){
+        if (!animationCompleted) {
+            log('start hiding')
+        } else {
+            log('finished hiding')
+        }
+    }
+})
 }); //end document ready function
 
 
